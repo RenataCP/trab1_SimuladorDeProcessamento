ac564fe46a419f9467cec7136d9e7766d320b001
Renata Costa <renatacostamachado8@gmail.com>
2021-04-14 20:37:19 -0300

    struct+menu

--- T01.c
+++ T01.c
@@ -1,8 +1,9 @@
 // Simulador de Processamentos
 // Alunos: Renata Costa - Engenharia da Computacao
-
 #include <stdio.h>
 
+//Constantes para o Menu
+#define SAIR 0
 #define LISTAR 1
 #define DETALHES 2
 #define ATUAL 3
@@ -10,6 +11,7 @@
 #define RESET 5
 #define CICLO 6
 
+//Dado tipo Processo
 typedef struct 
 {
     char* nome;
@@ -17,26 +19,76 @@ typedef struct
     int tempoExecucao;
 
     struct TProcesso* seguinte;
-}TProcesso;
+}TProcesso; 
 
 typedef struct
 {
     TProcesso* inicio;
     int tamanho;    
-} Pilha;
+}TPilha;
+
+void Init(TPilha* pilha)
+{
+    pilha->inicio = NULL;
+    pilha->tamanho = 0;
+}
 
 void ExibeMenu()
 {
     printf("\nSelecione uma op%c%co:\n", 135,198);
-    printf("\t%d - Listar processos", LISTAR);
-    printf("\t%d - Listar processos detalahadamente", DETALHES);
-    printf("\t%d - Visualizar processo atual", ATUAL);
-    printf("\t%d - Adicionar processo", ADICIONAR);
-    printf("\t%d - Resetar pilha", RESET);
-    printf("\t%d - Avan%car ciclo", CICLO, 135);
+    printf("\t%d - Listar processos\n", LISTAR);
+    printf("\t%d - Listar processos detalahadamente\n", DETALHES);
+    printf("\t%d - Visualizar processo atual\n", ATUAL);
+    printf("\t%d - Adicionar processo\n", ADICIONAR);
+    printf("\t%d - Resetar pilha\n", RESET);
+    printf("\t%d - Avan%car ciclo\n", CICLO, 135);
+    printf("\t%d - Sair\n", SAIR);
 }
 
-void main()
+
+int main()
 {
-    ExibeMenu();
+    TPilha* execucao;
+    int opcao;
+
+    if((execucao = (TPilha*) malloc(sizeof(TPilha))) == NULL)
+    {
+        printf("Erro 0x001\n\t...Encerrando...");// o nome do erro eh so para fazer graca
+        return -1;
+    }
+    Init(execucao);
+    do
+    {
+        ExibeMenu(); 
+        scanf("%d", &opcao);
+
+        switch (opcao)
+        {
+        case SAIR:
+            // printf("\nSAIR");
+            break;
+        case LISTAR:
+            // printf("\nLISTAR");
+            break;
+        case DETALHES:
+            // printf("\nDETALHES");
+            break;
+        case ATUAL:
+            // printf("\nATUAL");
+            break;
+        case ADICIONAR:
+            // printf("\nADICIONAR");
+            break;
+        case RESET:
+            // printf("\nRESET");
+            break;
+        case CICLO:
+            // printf("\nCICLO");
+            break;
+        
+        default:
+            printf("\aOpcao invalida\n");
+            break;
+        }
+    } while (opcao != SAIR);
 }
